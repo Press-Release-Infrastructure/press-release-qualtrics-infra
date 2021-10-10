1adcc08c3e4ac7127fb76671449f0a920c24e84b
Lily Bhattacharjee <lbhattacharjee@berkeley.edu>
2021-10-10 14:42:18 -0700

    Supports prematurely ending survey if user scores less than X% on training / attention check blocks

--- survey_generators/qualtrics_survey_generator_qsf.py
+++ survey_generators/qualtrics_survey_generator_qsf.py
@@ -3,6 +3,7 @@ import json
 import math
 import sys
 import numpy as np
+import copy
 
 # The Mturk question: In order to get paid for the work you have done on this survey, you need to enter the following code in the box at the bottom of the Mechanical Turk page where you started once you close this survey.
 
@@ -13,20 +14,24 @@ import numpy as np
 np.random.seed(0)
 
 # survey settings
-all_titles = list(pd.read_csv("ra_data.csv", encoding = 'utf8')["Headline"])
+all_titles = list(pd.read_csv("../input_headlines/ra_data.csv", encoding = 'utf8')["Headline"])
 
-num_headlines = 50 # unique titles to be classified
-num_students = 5 # number of people taking this survey version
+num_headlines = 5 # 50 # unique titles to be classified
+num_students = 2 # 5 # number of people taking this survey version
 overlap = 0.2 # percent of headlines assigned to 1 respondent that will be duplicated
-training_length = 5 # number of training titles
+training_length = 3 # number of training titles
 training_headlines = ["Training headline {}".format(i) for i in range(training_length)]
-block_size = 10 # number of questions in a block (between attention-check)
+training_answers = [np.random.randint(4) for i in range(len(training_headlines))]
+block_size = 3 # number of questions in a block (between attention-check)
+
+training_thresh = 0.9
+attention_thresh = 0.9
 
 conditional = False
 
 survey_name = "MTurk Trial"
-assignments_name = "mturk_assignments.json"
-qsf_name = "mturk_trial_refactored.qsf"
+assignments_name = "../respondent_assignments/mturk_assignments.json"
+qsf_name = "../qsf_output/mturk_trial_refactored.qsf"
 
 titles = np.array(all_titles)
 
@@ -34,12 +39,14 @@ titles = np.array(all_titles)
 titles_per_student = math.ceil(num_headlines / ((1 - overlap) * num_students))
 uniques_per_student = math.floor(num_headlines / num_students)
 
-attention_check_length = 3 # number of questions in an attention-check block
+attention_check_length = 2 # number of questions in an attention-check block
 attention_check_headlines = [["Attention check headline {}, Block {}".format(i, j) for i in range(attention_check_length)] for j in range(math.ceil(titles_per_student / block_size))]
+calc_attention_thresh = [math.ceil(len(i) * attention_thresh) for i in attention_check_headlines]
+
 attention_check_answers = {}
 for i in attention_check_headlines:
 	for j in i:
-		attention_check_answers[j] = ["Yes", "Acquirer", "Acquired"]
+		attention_check_answers[j] = [np.random.randint(4), "Acquirer", "Acquired"]
 
 uniques_left = num_headlines - num_students * uniques_per_student
 uniques = [uniques_per_student for i in range(num_students)]
@@ -76,6 +83,50 @@ with open(assignments_name, 'w') as f:
 	json.dump(student_assignments_json, f, ensure_ascii = False, indent = 2)
 
 survey_info = {}
+
+# args needed: flow id, right operand, right operand
+branch_logic_template = {
+	"Type": "Branch",
+	"FlowID": "",
+	"Description": "New Branch",
+	"BranchLogic": {
+		"0": {
+			"0": {
+				"LogicType": "EmbeddedField",
+				"LeftOperand": "Score",
+				"Operator": "LessThan",
+				"RightOperand": "",
+				"_HiddenExpression": False,
+				"Type": "Expression",
+				"Description": "<span class=\"ConjDesc\">If</span>  <span class=\"LeftOpDesc\">Score</span> <span class=\"OpDesc\">Is Less Than</span> <span class=\"RightOpDesc\"> "" </span>"
+			},
+			"Type": "If"
+		},
+		"Type": "BooleanExpression"
+	}
+}
+
+end_survey = {
+	"Type": "EndSurvey",
+	"FlowID": "FL_-500"
+}
+
+set_score = {
+	"Type": "EmbeddedData",
+	"FlowID": "FL_0",
+	"EmbeddedData": [
+		{
+		"Description": "Score",
+		"Type": "Custom",
+		"Field": "Score",
+		"VariableType": "String",
+		"DataVisibility": [],
+		"AnalyzeText": False,
+		"Value": "${gr://SC_0/Score}"
+		}
+	]
+}
+
 survey_info["SurveyEntry"] = {
 	"SurveyID": "SV_eLnpGNWb3hM31cy",
 	"SurveyName": survey_name,
@@ -114,9 +165,7 @@ survey_info["SurveyElements"] = [
       "Payload": {
       	"Type": "Root",
       	"FlowID": "FL_1",
-        "Flow": [
-          
-        ],
+        "Flow": [],
         "Properties": {
           "Count": 3,
         },
@@ -158,12 +207,18 @@ survey_info["SurveyElements"] = [
       "SecondaryAttribute": None,
       "TertiaryAttribute": None,
       "Payload": {
-        "ScoringCategories": [],
+        "ScoringCategories": [
+          {
+            "ID": "SC_0",
+            "Name": "Score",
+            "Description": ""
+          }
+        ],
         "ScoringCategoryGroups": [],
         "ScoringSummaryCategory": None,
         "ScoringSummaryAfterQuestions": 0,
         "ScoringSummaryAfterSurvey": 0,
-        "DefaultScoringCategory": None,
+        "DefaultScoringCategory": "SC_0",
         "AutoScoringCategory": None
       }
     },
@@ -207,41 +262,7 @@ survey_info["SurveyElements"][0]["Payload"].append(
 	}
 )
 
-# # insert key 2 into payload
-# survey_info["SurveyElements"][0]["Payload"].append(
-# 	{
-# 		"Type": "Standard",
-# 		"SubType": "",
-# 		"Description": "Survey Body",
-# 		"ID": "BL_2",
-# 		# "BlockElements": [],
-# 		# "Options": {
-# 		# 	"BlockLocking": "false",
-# 		# 	"RandomizeQuestions": "false",
-# 		# 	"BlockVisibility": "Collapsed",
-# 		# }
-# 	}
-# )
-
 survey_elements = survey_info["SurveyElements"]
-# block_elements = survey_elements[0]["Payload"][0]["BlockElements"]
-
-# # description of survey
-# block_elements.append({
-# 	"Type": "Question",
-# 	"QuestionID": "QID1"
-# })
-
-# # student ID question
-# block_elements.append({
-# 	"Type": "Question",
-# 	"QuestionID": "QID2"
-# })
-
-# # page break
-# block_elements.append({
-# 	"Type": "Page Break",
-# })
 
 directions = """For each headline, we want you to: 
 a) Identify whether the headline is about an acquisition or merger. 
@@ -400,7 +421,49 @@ def add_cond_display(student_qid, sids):
 		conj = "Or"
 	return q_cond_display
 
-def create_question(curr_title, curr, disp_settings = []):
+def create_branch_logic(branch_logic_template, fl_id, thresh):
+	branch_logic_template_copy = copy.deepcopy(branch_logic_template)
+	branch_logic_template_copy["FlowID"] = "FL_{}".format(fl_id)
+	branch_logic_template_copy["Flow"] = [end_survey]
+	branch_logic_template_copy["BranchLogic"]["0"]["0"]["RightOperand"] = str(thresh)
+	branch_logic_template_copy["BranchLogic"]["0"]["0"]["Description"] = "<span class=\"ConjDesc\">If</span>  <span class=\"LeftOpDesc\">Score</span> <span class=\"OpDesc\">Is Less Than</span> <span class=\"RightOpDesc\"> {} </span>".format(thresh)
+	return branch_logic_template_copy
+
+def add_score(elem, train_ans = -1):
+	if train_ans != -1:
+		elem["Payload"]["GradingData"] = [
+			{
+				"ChoiceID": "1",
+				"Grades": {
+					"SC_0": 0
+				},
+				"index": 0
+			},
+			{
+				"ChoiceID": "2",
+				"Grades": {
+					"SC_0": 0
+				},
+				"index": 1
+			},
+			{
+				"ChoiceID": "3",
+				"Grades": {
+					"SC_0": 0
+				},
+				"index": 2
+			},
+			{
+				"ChoiceID": "4",
+				"Grades": {
+					"SC_0": 0
+				},
+				"index": 3
+			}
+		]
+		elem["Payload"]["GradingData"][train_ans]["Grades"]["SC_0"] = 1
+
+def create_question(curr_title, curr, disp_settings = [], train_ans = -1):
 	qid = "QID{}".format(curr)
 
 	survey_info["SurveyElements"][0]["Payload"].append({
@@ -508,6 +571,8 @@ def create_question(curr_title, curr, disp_settings = []):
 					"QuestionID": qid
 				},
 			}
+
+			add_score(elem, train_ans)
 		elif subpart == 2:
 			elem = {
 				"SurveyID": "SV_eLnpGNWb3hM31cy",
@@ -625,8 +690,18 @@ def create_question(curr_title, curr, disp_settings = []):
 # start with all training headlines
 curr = 2
 for t in list(training_title_to_student.keys()):
-	create_question(t, curr, list(range(num_students)))
+	create_question(t, curr, list(range(num_students)), training_answers[curr - 2])
 	curr += 1
+
+# set score embedded data
+flow_elements = survey_elements[1]["Payload"]["Flow"]
+flow_elements.append(set_score)
+
+# add branch logic to kick respondent out of survey after training q's
+training_thresh_num = math.ceil(training_thresh * training_length)
+fl_id = -1
+flow_elements.append(create_branch_logic(branch_logic_template, fl_id, training_thresh_num))
+fl_id -= 1
 
 num_blocks = len(attention_check_headlines)
 
@@ -663,8 +738,15 @@ for i in range(num_blocks):
 			# special display settings
 			create_question(c, curr, regular_headline_to_student[c])
 		else:
-			create_question(c, curr, list(range(num_students)))
+			create_question(c, curr, list(range(num_students)), attention_check_answers[c][0])
 		curr += 1
+
+	# set score embedded data
+	flow_elements.append(set_score)
+
+	attention_thresh_num = training_thresh_num + sum(calc_attention_thresh[:i + 1])
+	flow_elements.append(create_branch_logic(branch_logic_template, fl_id, attention_thresh_num))
+	fl_id -= 1
 
 # create the rest of the questions for the remaining regular headlines
 for student, remaining in student_assignments.items():
