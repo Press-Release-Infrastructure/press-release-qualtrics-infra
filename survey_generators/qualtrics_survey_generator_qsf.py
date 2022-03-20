da209d891db8ed7aecea88e0a1d238a4c10f7d94
Lily Bhattacharjee <lbhattacharjee@berkeley.edu>
2022-03-20 08:56:16 -0700

    Add 'TRAINING' to the training screens, training commentary font = Monaco

--- survey_generators/qualtrics_survey_generator_qsf.py
+++ survey_generators/qualtrics_survey_generator_qsf.py
@@ -24,7 +24,7 @@ training_length = int(config["settings"]["training_length"]) # number of trainin
 block_size = int(config["settings"]["block_size"]) # number of questions in a block (between attention-check)
 attention_check_length = int(config["settings"]["attention_check_length"]) # number of questions in an attention-check block
 follow_up_flag = bool(int(config["settings"]["follow_up_flag"]))
-print(follow_up_flag)
+# print(follow_up_flag)
 
 training_thresh_mc = float(config["settings"]["training_thresh_mc"])
 training_thresh_te = float(config["settings"]["training_thresh_te"])
@@ -468,6 +468,8 @@ for d in directions:
 		d_elems = list(training_flow_headlines_df.iloc[curr - 4][d_format_elements[curr - 4]])
 		d = d % tuple(d_elems)
 
+	if 4 <= curr <= 10:
+		d = "<b style = 'color: #6082B6;'>TRAINING IN PROGRESS</b><br><br><div style = 'font-family: Monaco;'>" + d + "</div>"
 	survey_elements.append({
 		"SurveyID": "SV_eLnpGNWb3hM31cy",
 		"Element": "SQ",
